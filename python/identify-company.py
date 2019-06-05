5c63e4a0e42eab41191fa0f1739df241f7050415
Roger Armstrong <roger@armstrongconsulting.net>
2019-06-05 10:52:44 +0200

    identify omcnpanies

---
+++ python/identify-company.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+
+headers = {"Accept": "application/json", "api_key": "8842ace2-e377-48d9-b129-f952950ea535"}
+response = requests.get("https://test.cofacecentraleurope.com/api/bi/v1/company?identifier_type=120&identifier_value=6551640402&country_iso_code=PL", headers=headers)
+if (response.status_code == 200):
+    print(json.dumps(response.json(), indent=2))
+else:
+    raise ValueError("server returned http status %d (%s)" % (response.status_code, response.text))
